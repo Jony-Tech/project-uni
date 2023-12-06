484104f4b5e857d3a3e4b167d933c9b02d5516e0
testusuario <jonyxt1063@gmail.com>
2023-12-06 11:58:52 -0600

    message-ready

--- script.js
+++ script.js
@@ -50,15 +50,47 @@ elVideo.addEventListener('play', async() => {
         canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height) 
 
         //dibujar las lineas
-        faceapi.draw.drawDetections(canvas, resizedDetections)
+        // faceapi.draw.drawDetections(canvas, resizedDetections)
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
         resizedDetections.forEach(detection => {
-            const box = detection.detection.box;
-            new faceapi.draw.DrawBox (box, {
-                label: Math.round(detection.age) + ' años ' + detection.gender
-            }).draw(canvas)
-        },100)
+            // const box = detection.detection.box;
+            const ageInfo = Math.round(detection.age) + ' years ' + detection.gender;
+            const expressionInfo =printHtml(detection.expressions);
+
+            
+            
+
+            // new faceapi.draw.DrawBox (box, {
+            //     label: Math.round(detection.age) + ' años ' + detection.gender
+            // }).draw(canvas)
+
+            document.getElementById('ageInfo').textContent = ageInfo;
+            document.getElementById('expressionInfo').textContent = expressionInfo;
+        },100);
+        
     })
+    function getExpresionesInfo(expresiones) {
+    // Convierte el objeto de expresiones en una cadena legible
+    const expresionesArray = Object.entries(expresiones).map(([expresion, probabilidad]) => {
+        return `${expresion}: ${Math.round(probabilidad * 100)}`;
+    });
+
+    // Combina las expresiones en una cadena
+    return expresionesArray.join(', ');
+    }
+    function printHtml(expressions){
+        let emotion = '';
+        let valueEmotion = 0
+
+        for(const e in expressions){
+            if(expressions[e] > valueEmotion){
+                valueEmotion = expressions[e];
+                emotion = e;
+            }
+        }
+        return `${emotion} : ${valueEmotion}`
+    }
 })
+//neutral: 96%, happy: 2%, sad: 0%, angry: 1%, fearful: 0%, disgusted: 0%, surprised: 0%
